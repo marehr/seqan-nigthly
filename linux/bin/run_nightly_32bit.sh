4a41b617271609eb7fa8f3a809292aa5351ec5a7
holtgrew <holtgrew@2f57493d-8f5d-4ae1-8f3d-bc723045a211>
2012-06-11 11:35:41 +0000

    Changing used compilers.
    git-svn-id: https://svn.imp.fu-berlin.de/seqan-nightly/trunk@3 2f57493d-8f5d-4ae1-8f3d-bc723045a211

--- linux/bin/run_nightly_32bit.sh
+++ linux/bin/run_nightly_32bit.sh
@@ -8,12 +8,15 @@ mkdir -p ${DIR}/../log
 
 pushd ${DIR}/../cmake
 #CXX="g++-4.2" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.2-32.log
-#CXX="g++-4.3 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.3-32.log
-#CXX="g++-4.4 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.4-32.log
-#CXX="g++-4.5 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.5-32.log
-#CXX="g++-4.6 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-32.log
-#CXX="clang++-2.9 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-2.9-32.log
-#CXX="clang++-trunk -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-trunk-32.log
-CXX="g++-4.4 -m32" ctest -S seqan_linux_host_32bit.cmake,NightlyCoverage -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.4-32-coverage.log
-CXX="g++-4.4 -m32" ctest -S seqan_linux_host_32bit.cmake,NightlyMemCheck -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.4-32-memcheck.log
+CXX="g++-4.3 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.3-32.log
+CXX="g++-4.4 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-32.log
+CXX="g++-4.5 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.5-32.log
+CXX="g++-4.6 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-32.log
+CXX="g++-4.7 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.7-32.log
+CXX="clang++-2.9 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-2.9-32.log
+CXX="clang++-3.0 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-3.0-32.log
+CXX="clang++-3.1 -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-3.1-32.log
+CXX="clang++-trunk -m32" ctest -S seqan_linux_host_32bit.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-trunk-32.log
+CXX="g++-4.6 -m32" ctest -S seqan_linux_host_32bit.cmake,NightlyCoverage -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-32-coverage.log
+CXX="g++-4.6 -m32" ctest -S seqan_linux_host_32bit.cmake,NightlyMemCheck -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-32-memcheck.log
 popd
--- linux/bin/run_nightly_64bit.sh
+++ linux/bin/run_nightly_64bit.sh
@@ -8,13 +8,16 @@ mkdir -p ${DIR}/../log
 
 pushd ${DIR}/../cmake
 #CXX="g++-4.2" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.2-64.log
-#CXX="g++-4.3" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.3-64.log
-#CXX="g++-4.4" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.4-64.log
-#CXX="g++-4.5" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.5-64.log
-#CXX="g++-4.6" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-64.log
-#CXX="clang++-2.9" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-2.9-64.log
-#CXX="clang++-trunk" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-trunk-64.log
-#CXX="g++-4.4" ctest -S seqan_linux_host.cmake,NightlyCoverage -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.4-64-coverage.log
-#CXX="g++-4.4" ctest -S seqan_linux_host.cmake,NightlyMemCheck -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.4-64-memcheck.log
+CXX="g++-4.3" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.3-64.log
+CXX="g++-4.4" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-64.log
+CXX="g++-4.5" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.5-64.log
+CXX="g++-4.6" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-64.log
+CXX="g++-4.7" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.7-64.log
+CXX="clang++-2.9" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-2.9-64.log
+CXX="clang++-3.0" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-3.0-64.log
+CXX="clang++-3.1" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-3.1-64.log
+CXX="clang++-trunk" ctest -S seqan_linux_host.cmake,Nightly -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_clang++-trunk-64.log
+CXX="g++-4.6" ctest -S seqan_linux_host.cmake,NightlyCoverage -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-64-coverage.log
+CXX="g++-4.6" ctest -S seqan_linux_host.cmake,NightlyMemCheck -VV -d 2>&1 | tee ${DIR}/../log/ctest_nightly_g++-4.6-64-memcheck.log
 popd
 
