e5e864e7e2aa118dc1a304d941630fe448ade2f0
holtgrew <holtgrew@2f57493d-8f5d-4ae1-8f3d-bc723045a211>
2013-11-21 17:24:21 +0000

    Adding VS 12.
    git-svn-id: https://svn.imp.fu-berlin.de/seqan-nightly/trunk@40 2f57493d-8f5d-4ae1-8f3d-bc723045a211

--- windows/bin/experimental-all.bat
+++ windows/bin/experimental-all.bat
@@ -2,12 +2,14 @@
 call setvars-experimental.bat
 
 pushd c:\seqan-nightly\cmake
+call ctest -S seqan_win_vs12.cmake,branch=master,Experimental  -VV | mtee c:\seqan-nightly\log\seqan-experimental-master-vs12.log
 call ctest -S seqan_win_vs11.cmake,branch=master,Experimental  -VV | mtee c:\seqan-nightly\log\seqan-experimental-master-vs11.log
 call ctest -S seqan_win_vs10.cmake,branch=master,Experimental  -VV | mtee c:\seqan-nightly\log\seqan-experimental-master-vs10.log
 call ctest -S seqan_win_vs9.cmake,branch=master,Experimental   -VV | mtee c:\seqan-nightly\log\seqan-experimental-master-vs9.log
 call ctest -S seqan_win_vs8.cmake,branch=master,Experimental   -VV | mtee c:\seqan-nightly\log\seqan-experimental-master-vs8.log
 call ctest -S seqan_win_mingw.cmake,branch=master,Experimental -VV | mtee c:\seqan-nightly\log\seqan-experimental-master-mingw.log
 
+call ctest -S seqan_win_vs12.cmake,branch=develop,Experimental  -VV | mtee c:\seqan-nightly\log\seqan-experimental-develop-vs12.log
 call ctest -S seqan_win_vs11.cmake,branch=develop,Experimental  -VV | mtee c:\seqan-nightly\log\seqan-experimental-develop-vs11.log
 call ctest -S seqan_win_vs10.cmake,branch=develop,Experimental  -VV | mtee c:\seqan-nightly\log\seqan-experimental-develop-vs10.log
 call ctest -S seqan_win_vs9.cmake,branch=develop,Experimental   -VV | mtee c:\seqan-nightly\log\seqan-experimental-develop-vs9.log
--- windows/bin/nightly-all.bat
+++ windows/bin/nightly-all.bat
@@ -2,12 +2,14 @@
 call setvars-nightly.bat
 
 pushd c:\seqan-nightly\cmake
+call ctest -S seqan_win_vs12.cmake,branch=master,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs12.log
 call ctest -S seqan_win_vs11.cmake,branch=master,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs11.log
 call ctest -S seqan_win_vs10.cmake,branch=master,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs10.log
 call ctest -S seqan_win_vs9.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs9.log
 call ctest -S seqan_win_vs8.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs8.log
 call ctest -S seqan_win_mingw.cmake,branch=master,Nightly -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-mingw.log
 
+call ctest -S seqan_win_vs12.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs12.log
 call ctest -S seqan_win_vs11.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs11.log
 call ctest -S seqan_win_vs10.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs10.log
 call ctest -S seqan_win_vs9.cmake,branch=develop,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs9.log
