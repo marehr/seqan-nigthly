9e2af12b0591a2663d029b2660fe63ea11efaf0f
(no author) <(no author)@2f57493d-8f5d-4ae1-8f3d-bc723045a211>
2014-01-24 15:40:56 +0000

    added separate config files for different platforms; script improvements
    git-svn-id: https://svn.imp.fu-berlin.de/seqan-nightly/trunk@45 2f57493d-8f5d-4ae1-8f3d-bc723045a211

--- knime_packaging/build-plugin.sh
+++ knime_packaging/build-plugin.sh
@@ -7,9 +7,11 @@ source functions.sh
 # build the plugin source code
 pushd ${BASE_PLUGIN_PATH}
 
-ant -Dplugin.dir=${PLUGIN_SOURCE} -Dcustom.plugin.generator.target=${PLUGIN_BUILD} -Dknime.sdk=${KNIME_SDK}
+mkdir -p ${PLUGIN_BUILD}
+
+ant -Dplugin.dir=${PLUGIN_CENTRAL_PATH} -Dcustom.plugin.generator.target=${PLUGIN_BUILD} -Dknime.sdk=${KNIME_SDK}
 
 popd
 
 # copy to final, remote-accessible target
-rsync -avz --delete --exclude ".git" ${PLUGIN_BUILD}/ ${TARGET}
+rsync -avzn --delete --exclude ".git" ${PLUGIN_BUILD}/ ${REMOTE_TARGET}
--- knime_packaging/build-seqan.sh
+++ knime_packaging/build-seqan.sh
@@ -8,14 +8,20 @@ source functions.sh
 git_update $SEQAN_SOURCE master
 
 # clear old plugin sources
-rm -r $PLUGIN_SOURCE/*
+rm -r $PLUGIN_SOURCE
+mkdir -p $PLUGIN_SOURCE
 
 # configure seqan
+mkdir -p $SEQAN_BUILD
 cd $SEQAN_BUILD
 cmake -D WORKFLOW_PLUGIN_DIR=$PLUGIN_SOURCE -D CMAKE_BUILD_TYPE=Release $SEQAN_SOURCE
 
 # build seqan
-cmake --build . --target prepare_workflow_plugin --config Release
+#cmake --build . --target prepare_workflow_plugin --config Release
+make -j10 prepare_workflow_plugin
+
+# ensure plugin central exists
+ssh $PLUGIN_CENTRAL_HOST "mkdir -p ${PLUGIN_CENTRAL_PATH}"
 
 # copy to central assembly location
 rsync -avz $PLUGIN_SOURCE/ $PLUGIN_CENTRAL/
