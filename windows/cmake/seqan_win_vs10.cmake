afa149b41d8a4b7e90ee1682f196941f8c207443
(no author) <(no author)@2f57493d-8f5d-4ae1-8f3d-bc723045a211>
2016-01-18 15:02:01 +0000

    Fixed cmake generator name of VS versions.
    git-svn-id: https://svn.imp.fu-berlin.de/seqan-nightly/trunk@73 2f57493d-8f5d-4ae1-8f3d-bc723045a211

--- windows/cmake/seqan_win_vs10.cmake
+++ windows/cmake/seqan_win_vs10.cmake
@@ -1,5 +1,5 @@
 # See README.txt for the strategy used in the CMake files.
 cmake_policy (SET CMP0011 NEW)  # Suppress warning about PUSH/POP policy change.
 
-SET (SEQAN_CTEST_GENERATOR "Visual Studio 10")
+SET (SEQAN_CTEST_GENERATOR "Visual Studio 10 2011")
 INCLUDE (seqan_win_host.cmake)
--- windows/cmake/seqan_win_vs11.cmake
+++ windows/cmake/seqan_win_vs11.cmake
@@ -1,5 +1,5 @@
 # See README.txt for the strategy used in the CMake files.
 cmake_policy (SET CMP0011 NEW)  # Suppress warning about PUSH/POP policy change.
 
-SET (SEQAN_CTEST_GENERATOR "Visual Studio 11")
+SET (SEQAN_CTEST_GENERATOR "Visual Studio 11 2012")
 INCLUDE (seqan_win_host.cmake)
