3e66e9b93a4ae4e03d65be284fabafe1396a03ae
holtgrew <holtgrew@2f57493d-8f5d-4ae1-8f3d-bc723045a211>
2016-02-02 20:02:14 +0000

    Enabled vs 14 on master build
    git-svn-id: https://svn.imp.fu-berlin.de/seqan-nightly/trunk@75 2f57493d-8f5d-4ae1-8f3d-bc723045a211

--- windows/bin/nightly-all.bat
+++ windows/bin/nightly-all.bat
@@ -3,7 +3,7 @@ call setvars-nightly.bat
 
 pushd c:\seqan-nightly\cmake
 call ctest -S seqan_win_vs14.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs14.log
-REM call ctest -S seqan_win_vs14.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs14.log
+call ctest -S seqan_win_vs14.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs14.log
 REM call ctest -S seqan_win_vs12.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs12.log
 call ctest -S seqan_win_vs12.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs12.log
 REM call ctest -S seqan_win_vs11.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs11.log
