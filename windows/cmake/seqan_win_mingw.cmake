04753e054229bac96cf52d8832761f2abb1eb485
marehr <marehr@2f57493d-8f5d-4ae1-8f3d-bc723045a211>
2016-06-09 13:00:51 +0000

    Cleanup old windows build platforms
    git-svn-id: https://svn.imp.fu-berlin.de/seqan-nightly/trunk@78 2f57493d-8f5d-4ae1-8f3d-bc723045a211

--- windows/cmake/seqan_win_mingw.cmake
+++
@@ -1,8 +0,0 @@
-# See README.txt for the strategy used in the CMake files.
-cmake_policy (SET CMP0011 NEW)  # Suppress warning about PUSH/POP policy change.
-
-# Make GCC expect 586 processor and thus generate real atomic 4-byte operations.
-SET (CTEST_CXX_FLAGS "-march=i586")
-
-SET (SEQAN_CTEST_GENERATOR "MinGW Makefiles")
-INCLUDE (seqan_win_host.cmake)
--- windows/cmake/seqan_win_vs8.cmake
+++
@@ -1,5 +0,0 @@
-# See README.txt for the strategy used in the CMake files.
-cmake_policy (SET CMP0011 NEW)  # Suppress warning about PUSH/POP policy change.
-
-SET (SEQAN_CTEST_GENERATOR "Visual Studio 8 2005")
-INCLUDE (seqan_win_host.cmake)
