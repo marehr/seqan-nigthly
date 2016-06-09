ba148d9b6e064dc160a877af9c245dcb0cbc8401
marehr <marehr@2f57493d-8f5d-4ae1-8f3d-bc723045a211>
2016-06-09 12:21:46 +0000

    Disables old vs versions for nightly builds.
    git-svn-id: https://svn.imp.fu-berlin.de/seqan-nightly/trunk@77 2f57493d-8f5d-4ae1-8f3d-bc723045a211

--- windows/bin/nightly-all.bat
+++ windows/bin/nightly-all.bat
@@ -5,11 +5,11 @@ pushd c:\seqan-nightly\cmake
 call ctest -S seqan_win_vs14.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs14.log
 call ctest -S seqan_win_vs14.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs14.log
 REM call ctest -S seqan_win_vs12.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs12.log
-call ctest -S seqan_win_vs12.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs12.log
+REM call ctest -S seqan_win_vs12.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs12.log
 REM call ctest -S seqan_win_vs11.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs11.log
-call ctest -S seqan_win_vs11.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs11.log
+REM call ctest -S seqan_win_vs11.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs11.log
 REM call ctest -S seqan_win_vs10.cmake,branch=develop,Nightly  -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs10.log
-call ctest -S seqan_win_vs10.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs10.log
+REM call ctest -S seqan_win_vs10.cmake,branch=master,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs10.log
 REM call ctest -S seqan_win_vs9.cmake,branch=develop,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs9.log
 REM call ctest -S seqan_win_vs9.cmake,branch=master,Nightly    -VV | mtee c:\seqan-nightly\log\seqan-nightly-master-vs9.log
 REM call ctest -S seqan_win_vs8.cmake,branch=develop,Nightly   -VV | mtee c:\seqan-nightly\log\seqan-nightly-develop-vs8.log
